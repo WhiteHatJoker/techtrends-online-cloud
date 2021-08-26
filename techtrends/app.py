1029632fb9eb967a08a0312ebf53071a0fb4b558
Ravshan <rkhudoyberdiev@gmail.com>
2021-08-27 03:48:22 +0500

    Added healz endpoint with connection checks

--- techtrends/app.py
+++ techtrends/app.py
@@ -10,10 +10,33 @@ db_conn_counter = 0
 # This function connects to database with the name `database.db`
 def get_db_connection():
     global db_conn_counter
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    db_conn_counter += 1
-    return connection
+    try:
+        connection = sqlite3.connect('database.db')
+        print(connection)
+        connection.row_factory = sqlite3.Row
+        connection.cursor()
+        db_conn_counter += 1
+        return connection
+    except:
+        return False
+
+# Function to check the connection
+def check_connection(conn):
+    try:
+        conn.cursor()
+        return True
+    except Exception as ex:
+        return False
+
+# Function to check if posts table exiists
+def posts_table_exists(conn):
+    c = conn.cursor()
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='posts' ''')
+    if c.fetchone()[0]==1 : 
+        return True
+    else:
+        return False
+
 
 # Function to get a post using its ID
 def get_post(post_id):
@@ -73,16 +96,26 @@ def create():
 # Define healz endpoint for checking the app health
 @app.route('/healz')
 def health():
+    default_status_text = "OK - healthy"
+    default_status = 200
+    connection = get_db_connection()
+    # To check the failure in case connection to database closes
+    # connection.close()
+
+    # Check if the connection is good  -- Standout
+    if not check_connection(connection) or not posts_table_exists(connection):
+        default_status_text = "ERROR - unhealthy"
+        default_status = 500
+
     response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"result":default_status_text}),
+        status=default_status,
+        mimetype='application/json'
     )
 
     return response
 
 # Define the metrics endpoint
-@app.route('/metrics')
 
 # start the application on port 3111
 if __name__ == "__main__":
